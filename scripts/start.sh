7d6049540e7ae9cd926155b32ea4f7de3cf985a1
Nils Van den Steen <nvandensteen@naturalsciences.be>
2024-02-01 15:01:30 +0100

    [ADD] init commit

---
+++ scripts/start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+GH_OWNER=$GH_OWNER
+GH_REPOSITORY=$GH_REPOSITORY
+GH_TOKEN=$GH_TOKEN
+
+RUNNER_SUFFIX=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 5 | head -n 1)
+RUNNER_NAME="dockerNode-${RUNNER_SUFFIX}"
+
+# this was included in the tutorial
+# but I get the token of the runner from the web interface
+# REG_TOKEN=$(curl -sX POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GH_TOKEN}" https://api.github.com/repos/${GH_OWNER}/${GH_REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
+
+cd /home/docker/actions-runner
+
+echo "URL:https://github.com/${GH_OWNER}/${GH_REPOSITORY} --token ${REG_TOKEN} --name ${RUNNER_NAME}"
+echo "$(ls -lt)"
+ 
+./config.sh --unattended --url https://github.com/${GH_OWNER}/${GH_REPOSITORY} --token ${GH_TOKEN} --name ${RUNNER_NAME}
+
+cleanup() {
+    echo "Removing runner..."
+    ./config.sh remove --unattended --token ${REG_TOKEN}
+}
+
+trap 'cleanup; exit 130' INT
+trap 'cleanup; exit 143' TERM
+
+./run.sh & wait $!
